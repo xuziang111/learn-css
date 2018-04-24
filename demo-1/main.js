544848104cfbea4f046553d2ac1260334db7610f
xuziang111 <584788096@qq.com>
2018-04-24 17:25:34 +0800

    demo-1

---
+++ demo-1/main.js
@@ -0,0 +1,19 @@
+switchButton = document.getElementById("switchButton")
+switchTitle = document.getElementById("switchTitle")
+
+switchButton.addEventListener('mousedown',()=>{
+    setTimeout(()=>{switchButton.className="active"},0)
+    
+})
+switchButton.addEventListener('mouseup',()=>{
+    switchButton.className=""
+})
+
+switchButton.addEventListener('click',()=>{
+    console.log(switchTitle.className)
+    if(switchTitle.className!=="active"){
+        switchTitle.className="active"
+    }else{
+        switchTitle.className=""
+    }
+})
